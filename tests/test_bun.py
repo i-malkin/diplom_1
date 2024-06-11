cfdcfb83b5959fb8fc63866411d39aea9da13b16
Malkin Ilya <i-malkin@ya.ru>
2024-06-11 21:48:59 +0400

    1.Правка: разбила на 2 отдельных теста

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -1,12 +1,22 @@
 import pytest
 from praktikum.bun import Bun
 
-@pytest.mark.parametrize("name, price", [
-    ("Test Bun 1", 50.0),
-    ("Test Bun 2", 75.0),
-    ("Test Bun 3", 100.0)
-])
-def test_bun_creation(name, price):
-    bun = Bun(name, price)
-    assert bun.get_name() == name
-    assert bun.get_price() == price
+class TestBun:
+
+    @pytest.mark.parametrize("name, price", [
+        ("Test Bun 1", 50.0),
+        ("Test Bun 2", 75.0),
+        ("Test Bun 3", 100.0)
+    ])
+    def test_bun_get_name(self, name, price):
+        bun = Bun(name, price)
+        assert bun.get_name() == name
+
+    @pytest.mark.parametrize("name, price", [
+        ("Test Bun 1", 50.0),
+        ("Test Bun 2", 75.0),
+        ("Test Bun 3", 100.0)
+    ])
+    def test_bun_get_price(self, name, price):
+        bun = Bun(name, price)
+        assert bun.get_price() == price
