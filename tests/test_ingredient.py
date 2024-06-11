c5062b16b50e100a3a605ae2cd1a9c5e91aaa20d
Malkin Ilya <i-malkin@ya.ru>
2024-06-11 21:59:17 +0400

    Правка: тесты разбиты по принципу один тест - один метод проверяем. Тесты объединены в классы.

--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -2,13 +2,30 @@ import pytest
 from praktikum.ingredient import Ingredient
 from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
 
-@pytest.mark.parametrize("ingredient_type, name, price", [
-    (INGREDIENT_TYPE_SAUCE, "Hot Sauce", 10.0),
-    (INGREDIENT_TYPE_FILLING, "Patty", 20.0),
-    (INGREDIENT_TYPE_SAUCE, "Ketchup", 5.0)
-])
-def test_ingredient_creation(ingredient_type, name, price):
-    ingredient = Ingredient(ingredient_type, name, price)
-    assert ingredient.get_type() == ingredient_type
-    assert ingredient.get_name() == name
-    assert ingredient.get_price() == price
+class TestIngredientCreation:
+
+    @pytest.mark.parametrize("ingredient_type", [
+        (INGREDIENT_TYPE_SAUCE),
+        (INGREDIENT_TYPE_FILLING)
+    ])
+    def test_ingredient_type(self, ingredient_type):
+        ingredient = Ingredient(ingredient_type, "Test Ingredient", 10.0)
+        assert ingredient.get_type() == ingredient_type
+
+    @pytest.mark.parametrize("name", [
+        ("Hot Sauce"),
+        ("Patty"),
+        ("Ketchup")
+    ])
+    def test_ingredient_name(self, name):
+        ingredient = Ingredient(INGREDIENT_TYPE_SAUCE, name, 10.0)
+        assert ingredient.get_name() == name
+
+    @pytest.mark.parametrize("price", [
+        (10.0),
+        (20.0),
+        (5.0)
+    ])
+    def test_ingredient_price(self, price):
+        ingredient = Ingredient(INGREDIENT_TYPE_SAUCE, "Test Ingredient", price)
+        assert ingredient.get_price() == price
